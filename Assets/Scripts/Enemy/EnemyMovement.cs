e53e2d61595625d27eebc1430a19c0df24f5c2c5
nguyenhieundph <nguyendinhhieu11a6@gmail.com>
2026-07-21 10:05:09 +0700

    feat: add shared Health system with OnDeath/OnDamaged events

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Movement Settings")]
+    [SerializeField] private float _repathThreshold = 0.5f; // khoảng cách tối thiểu để gọi lại SetDestination
 
+    private NavMeshAgent _agent;
+    private EnemyDetection _detection;
+    private Vector3 _lastKnownPlayerPosition;
+
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _detection = GetComponent<EnemyDetection>();
+    }
     // Update is called once per frame
     void Update()
     {
-        
+       
+        if (_detection.CanSeePlayer == false)
+        {
+            StopMoving();
+
+            return;
+        }
+        else
+        {
+            Vector3 currentPlayerPos = _detection.PlayerTransform.position;
+            float distanceMoved = Vector3.Distance(_lastKnownPlayerPosition,currentPlayerPos);
+
+            if(distanceMoved > _repathThreshold)
+            {
+                _agent.SetDestination(currentPlayerPos);
+                _lastKnownPlayerPosition = currentPlayerPos;
+            }
+        }
+    }
+
+    private void StopMoving()
+    {
+        _agent.SetDestination(transform.position);
     }
 }
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System;
+
+public class Health : MonoBehaviour
+{
+    [Header("Health Settings")]
+    [SerializeField] private float _maxHealth = 100f;
+
+    public float CurrentHealth { get; private set; }
+    public bool IsDead { get; private set; }
+
+    public event Action OnDeath;
+    public event Action<float> OnDamaged;
+
+    void Awake()
+    {
+        CurrentHealth = _maxHealth;
+        IsDead = false;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        if(IsDead == true)
+        {
+            return;
+        }
+
+        CurrentHealth -= amount;
+        CurrentHealth = Mathf.Max(CurrentHealth, 0);
+      
+        OnDamaged?.Invoke(amount);
+
+        if (CurrentHealth <= 0) { 
+            IsDead = true;
+            OnDeath?.Invoke();
+        }
+    }
+}
